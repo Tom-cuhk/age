59b76a04eca63b10ee35715619768a15cff5f43f
Tom Chan <tchan.cuhk@gmail.com>
2021-05-30 23:28:44 +0800

    updated version

--- age.py
+++ age.py
@@ -1,4 +1,8 @@
 driving = input('Do you have driving licence?')
+if driving != 'yes' and driving != 'no':
+	print ('You should only type yes/no.')
+	raise SystemExit
+
 age = input('How old are you?')
 age = int(age)
 if driving == 'yes':
@@ -6,8 +10,8 @@ if driving == 'yes':
 		print('You are a qualified driver!')
 	else:
 		print('You are too young to drive!')
-else:
+elif driving == 'no':
 	if age >= 18:
 		print('Do you consider to apply a driving licence?')
-	elif age <18:
+	else:
 		print('You could apply for driving licence when you are 18 years old.')	
